39da2ee8ce52abf5fde9d93757b3f0d315981572
macszl <235989@edu.p.lodz.pl>
2022-01-11 15:30:51 +0100

    fixed a bug

--- src/main.c
+++ src/main.c
@@ -48,31 +48,26 @@ typedef struct beast_tracker_t
 } beast_tracker_t;
 typedef struct coin_spawn_manager_t
 {
-    map_point_t spawner_array[COIN_SPAWNER_NUM];
+    map_point_t * spawner_array[COIN_SPAWNER_NUM];
     int free_spawners;
-    int total_spawners;
 } coin_spawn_manager_t;
 
 typedef struct beast_spawn_manager_t
 {
-    map_point_t spawner_array[BEAST_SPAWNER_NUM];
+    map_point_t * spawner_array[BEAST_SPAWNER_NUM];
     int free_spawners;
-    int total_spawners;
 } beast_spawn_manager_t;
 
 void spawn_coin(entity_t coin_size, map_point_t map[]);
 int coin_spawn_init(map_point_t map[]);
-int coin_spawn_occ(map_point_t occupied_point);
-int coin_spawn_free(map_point_t freed_point);
-int coin_spawn_search_for_element_internal(map_point_t element);
 
-int spawn_beast(map_point_t map[], beast_tracker_t beast_tracker);
+int spawn_beast(beast_tracker_t * beast_tracker);
 int beast_spawn_init(map_point_t map[]);
-int beast_move(map_point_t map[], beast_tracker_t beast_tracker, map_point_t * beast);
-int handle_beasts(map_point_t map[], beast_tracker_t beast_tracker);
+int beast_move(map_point_t map[], beast_tracker_t * beast_tracker, map_point_t * beast);
+int handle_beasts(map_point_t map[], beast_tracker_t * beast_tracker);
 
 void *input_routine(void *input_storage);
-void handle_event(int c, map_point_t map[], beast_tracker_t beast_tracker);
+void handle_event(int c, map_point_t map[], beast_tracker_t * beast_tracker);
 
 coin_spawn_manager_t coinSpawnManager;
 beast_spawn_manager_t beastSpawnManager;
@@ -93,7 +88,7 @@ int main() {
     if(err != 0)
         return 1;
 
-    beast_tracker_t beast_tracker = {.size = 0};
+    beast_tracker_t beast_tracker = { .size = 0};
     coin_spawn_init(map);
     beast_spawn_init(map);
 
@@ -151,8 +146,8 @@ int main() {
         render_map(map, window);
         wrefresh(window);
         int c = getch();
-        handle_event(c, map, beast_tracker);
-        handle_beasts(map, beast_tracker);
+        handle_event(c, map, &beast_tracker);
+        handle_beasts(map, &beast_tracker);
         i++;
     }
 
@@ -165,7 +160,7 @@ int main() {
 }
 
 //Keystroke handling: adding entities based on which key was pressed
-void handle_event(int c, map_point_t map[], beast_tracker_t beast_tracker)
+void handle_event(int c, map_point_t map[], beast_tracker_t * beast_tracker)
 {
     switch (c) {
         //arrow key handling: works only with unix distros as unix represents arrow keys as 3 characters
@@ -180,7 +175,7 @@ void handle_event(int c, map_point_t map[], beast_tracker_t beast_tracker)
         case 'B':
         case 'b':
         {
-            spawn_beast(map, beast_tracker);
+            spawn_beast(beast_tracker);
             break;
         }
         case 'c':
@@ -247,48 +242,67 @@ int beast_spawn_init(map_point_t map[])
     }
 
     beastSpawnManager.free_spawners = (int) free_spawners;
-    beastSpawnManager.total_spawners = (int) total_spawners;
 
     int arr_cnt = 0;
     for(int i = 0; i < MAP_WIDTH * MAP_LENGTH; i++)
     {
         if( map[i].entity_type == ENTITY_FREE && map[i].spawnerType == BEAST_SPAWNER)
         {
-            memcpy( &coinSpawnManager.spawner_array[arr_cnt], &map[i], sizeof(map_point_t));
+            beastSpawnManager.spawner_array[arr_cnt] = &map[i];
             arr_cnt++;
         }
     }
     return 0;
 }
-int spawn_beast(map_point_t map[], beast_tracker_t beast_tracker)
+int spawn_beast(beast_tracker_t * beast_tracker)
 {
+    if(beast_tracker->size < 0 ||beast_tracker->size == TOTAL_BEAST_LIMIT || beastSpawnManager.free_spawners == 0)
+    {
+        return -1;
+    }
     int which_beast_spawner = rand() % beastSpawnManager.free_spawners;
-
-    beastSpawnManager.spawner_array[which_beast_spawner].entity_type = ENTITY_BEAST;
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
+    beastSpawnManager.spawner_array[which_beast_spawner]->entity_type = ENTITY_BEAST;
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
     int loc = beastSpawnManager.free_spawners - 1;
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
     swap(beastSpawnManager.spawner_array[which_beast_spawner], beastSpawnManager.spawner_array[loc]);
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
     beastSpawnManager.free_spawners--;
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
 
-
-    int b_x = (int) beastSpawnManager.spawner_array[loc].point.x;
-    int b_y = (int) beastSpawnManager.spawner_array[loc].point.y;
-    beast_tracker.beasts[beast_tracker.size] = &map[ b_y * MAP_WIDTH + b_x];
-    beast_tracker.size++;
-
+    beast_tracker->beasts[beast_tracker->size] = beastSpawnManager.spawner_array[loc];
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
+    beast_tracker->size = beast_tracker->size + 1;
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
     return 0;
 }
-int beast_move( map_point_t map[],  beast_tracker_t beast_tracker, map_point_t * beast)
+int beast_move( map_point_t map[],  beast_tracker_t * beast_tracker, map_point_t * beast)
 {
     //TODO Player collision, beast_spawner_occupation
     //error checking
-    if( beast->entity_type != ENTITY_BEAST)
+    if( beast->entity_type != ENTITY_BEAST || beast_tracker->size < 0) {
         return -1;
-
+    }
     int beast_tracker_index = 0;
     bool beast_found = false;
-    for(int i = 0; i < beast_tracker.size; i++)
+    for(int i = 0; i < beast_tracker->size; i++)
     {
-        if( beast_tracker.beasts[i] == beast)
+        if( beast_tracker->beasts[i] == beast)
         {
             beast_found = true;
             break;
@@ -303,15 +317,18 @@ int beast_move( map_point_t map[],  beast_tracker_t beast_tracker, map_point_t *
     map_point_t * possible_locs[4];
     int locs_cnt = 0;
     int b_x = (int) beast->point.x;
-    int b_y = (int) beast->point.x;
-    for(int i = 0; i < 1; i++)
+    int b_y = (int) beast->point.y;
+    for(int i = -1; i < 2; i++)
     {
-        for(int j = 0; j < 1; j++)
+        for(int j = -1; j < 2; j++)
         {
-            if( map[(b_y + i) * MAP_WIDTH + b_x + j].entity_type == ENTITY_FREE )
+            if( (i == -1 && j == 0) || (i == 1 && j == 0) || (i == 0 && j == -1) || (i == 0 && j == 1)  )
             {
-                possible_locs[locs_cnt] = &map[(b_y + i) * MAP_WIDTH + b_x + j];
-                locs_cnt++;
+                if (map[(b_y + i) * MAP_WIDTH + b_x + j].entity_type == ENTITY_FREE)
+                {
+                    possible_locs[locs_cnt] = &map[(b_y + i) * MAP_WIDTH + b_x + j];
+                    locs_cnt++;
+                }
             }
         }
     }
@@ -322,15 +339,18 @@ int beast_move( map_point_t map[],  beast_tracker_t beast_tracker, map_point_t *
     possible_locs[which_loc]->entity_type = ENTITY_BEAST;
     beast->entity_type = ENTITY_FREE;
 
-    beast_tracker.beasts[beast_tracker_index] = possible_locs[which_loc];
+    beast_tracker->beasts[beast_tracker_index] = possible_locs[which_loc];
     return 0;
 }
 
-int handle_beasts(map_point_t map[], beast_tracker_t beast_tracker)
+int handle_beasts(map_point_t map[], beast_tracker_t * beast_tracker)
 {
-    for(int i = 0; i < beast_tracker.size; i++)
+    if(beast_tracker->size < 0) {
+        return -1;
+    }
+    for(int i = 0; i < beast_tracker->size; i++)
     {
-        beast_move(map, beast_tracker, beast_tracker.beasts[i]);
+        beast_move(map, beast_tracker, beast_tracker->beasts[i]);
     }
     return 0;
 }
@@ -339,102 +359,29 @@ int handle_beasts(map_point_t map[], beast_tracker_t beast_tracker)
 // Function that initializes the coinManager variable which allows oversight over all the coin spawns over the map
 int coin_spawn_init(map_point_t map[])
 {
-    unsigned long total_spawners = 0;
     unsigned long free_spawners = 0;
     for(int i = 0; i < MAP_WIDTH * MAP_LENGTH; i++)
     {
         if( map[i].entity_type == ENTITY_FREE && map[i].spawnerType == COIN_SPAWNER)
         {
-            total_spawners++;
             free_spawners++;
         }
     }
 
     coinSpawnManager.free_spawners = (int) free_spawners;
-    coinSpawnManager.total_spawners = (int) total_spawners;
 
     int arr_cnt = 0;
     for(int i = 0; i < MAP_WIDTH * MAP_LENGTH; i++)
     {
         if( map[i].entity_type == ENTITY_FREE && map[i].spawnerType == COIN_SPAWNER)
         {
-            memcpy( &coinSpawnManager.spawner_array[arr_cnt], &map[i], sizeof(map_point_t));
+            coinSpawnManager.spawner_array[arr_cnt] = &map[i];
             arr_cnt++;
         }
     }
     return 0;
 }
 
-//TODO Refactor this kludge into something prettier
-
-//returns the position of a map_point_t element, returns -1 if not found
-int coin_spawn_search_for_element_internal(map_point_t element)
-{
-    map_point_t * arr = coinSpawnManager.spawner_array;
-    int i = 0; bool element_found = false;
-    for(;i < coinSpawnManager.total_spawners; i++)
-    {
-        if( element.point.x == arr[i].point.x  && element.point.y == arr[i].point.y)
-        {
-            element_found = true;
-            break;
-        }
-    }
-    if( !element_found)
-        return -1;
-    else
-        return i;
-}
-int coin_spawn_occ(map_point_t occupied_point)
-{
-    if(coinSpawnManager.free_spawners == 0)
-        return -1;
-
-    //znajdowanie elementu
-    int pos = coin_spawn_search_for_element_internal(occupied_point);
-    if(pos == -1)
-        return -1;
-
-    //swap znalezionego elemetu z ostatnim wolnym elementem
-    int last_free_pos = coinSpawnManager.free_spawners - 1;
-#if DEBUG
-    map_point_t test1 = coinSpawnManager.spawner_array[last_free_pos];
-    map_point_t test2 = coinSpawnManager.spawner_array[pos];
-    if(test1.point.y > 32 || test1.point.y < 0 || test1.point.x > 32 || test1.point.x < 0 ||
-            test2.point.y > 32 || test2.point.y < 0 || test2.point.x > 32 || test2.point.x < 0)
-    {
-        printf( " a");
-    }
-#endif
-    swap(coinSpawnManager.spawner_array[last_free_pos], coinSpawnManager.spawner_array[pos] );
-    coinSpawnManager.free_spawners--;
-    return 0;
-}
-int coin_spawn_free(map_point_t freed_point)
-{
-    if(coinSpawnManager.free_spawners == coinSpawnManager.total_spawners)
-        return -1;
-
-    //znajdowanie elementu
-    int pos = coin_spawn_search_for_element_internal(freed_point);
-    if(pos == -1)
-        return -1;
-
-    //swap znalezionego elemetu z ostatnim zajetym elementem
-    int last_occ_pos = coinSpawnManager.free_spawners;
-#if DEBUG
-    map_point_t test1 = coinSpawnManager.spawner_array[last_occ_pos];
-    map_point_t test2 = coinSpawnManager.spawner_array[pos];
-    if(test1.point.y > 32 || test1.point.y < 0 || test1.point.x > 32 || test1.point.x < 0 ||
-       test2.point.y > 32 || test2.point.y < 0 || test2.point.x > 32 || test2.point.x < 0)
-    {
-        printf( " a");
-    }
-#endif
-    swap(coinSpawnManager.spawner_array[last_occ_pos], coinSpawnManager.spawner_array[pos]);
-    coinSpawnManager.free_spawners++;
-    return 0;
-}
 void spawn_coin(entity_t coin_size, map_point_t map[])
 {
     if( coin_size < ENTITY_COIN_SMALL || coinSpawnManager.free_spawners == 0)
@@ -443,16 +390,18 @@ void spawn_coin(entity_t coin_size, map_point_t map[])
     }
 
     int which_spawner_to_use = rand() % coinSpawnManager.free_spawners;
-    int y = (int )coinSpawnManager.spawner_array[which_spawner_to_use].point.y;
-    int x = (int) coinSpawnManager.spawner_array[which_spawner_to_use].point.x;
+    int y = (int )coinSpawnManager.spawner_array[which_spawner_to_use]->point.y;
+    int x = (int) coinSpawnManager.spawner_array[which_spawner_to_use]->point.x;
 #if DEBUG
     if(x > 32 || y > 32 || y < 0 || x < 0 )
     {
         printf("a");
     }
 #endif
-    map_point_t temp;
-    memcpy(&temp, &coinSpawnManager.spawner_array[which_spawner_to_use], sizeof(map_point_t));
-    coin_spawn_occ(temp);
     map[y * MAP_WIDTH + x].entity_type = coin_size;
+
+    int free_spwn_cnt = coinSpawnManager.free_spawners;
+    swap(coinSpawnManager.spawner_array[which_spawner_to_use], coinSpawnManager.spawner_array[free_spwn_cnt - 1]);
+    coinSpawnManager.free_spawners--;
+
 }
